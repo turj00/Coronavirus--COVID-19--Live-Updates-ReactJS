c4c1510acb955d68f51ebbd0887320c26de7e6d7
turj00 <turjo1331@gmail.com>
2020-09-09 18:44:21 +0600

    Updates

---
+++ precache-manifest.46f9a2454ba9fa8a7e17f6b024df1a74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb5ab6e377ae0edc02c521debc73095",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/index.html"
+  },
+  {
+    "revision": "e9fc8c4195bf11d4e3c1",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/css/main.a47927b4.chunk.css"
+  },
+  {
+    "revision": "8e11ecd51ce0bc712b6d",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/js/2.e32fb41d.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/js/2.e32fb41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fc8c4195bf11d4e3c1",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/js/main.d74c362f.chunk.js"
+  },
+  {
+    "revision": "f5e35839f63046ab2daa",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/js/runtime-main.940c9a92.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/media/Covid19_3.d7265326.png"
+  },
+  {
+    "revision": "2ffdfbac9487df4fb0816bdf11916088",
+    "url": "/Coronavirus--COVID-19--Live-Updates-ReactJS/static/media/covid19_bk8.2ffdfbac.jpg"
+  }
+]);
